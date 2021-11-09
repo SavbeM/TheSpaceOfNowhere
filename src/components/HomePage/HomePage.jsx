67d3d84f6678e3674534c62a99937a0b4651b4a5
savbeM <mark.selihov2458@gmail.com>
2021-11-09 10:16:37 +0300

    active version

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,97 +3,160 @@ import "./HomePage.css"
 
 export let HomePage = (props) => {
     return (
-        <div>
-            <div className="homepage-welcome-content">
-                <div className='welcome-content'>
-                    <div className='welcome-title'>
-                        WELCOME
-                    </div>
-                    <div className="welcome-subtitle">
-                        We are a project that combines an online gallery with an NFT-marketplace. The mission of this project is to create a singular void space for viewing and exchanging your experience and observations, concepts and ideas, through the form of virtual artwork as it is, in our opinion the only singular thing that won't perish as long as the humankind exists. we hope you'll be able to find an artwork that may spark a creative idea inside of your mind.
-                    </div>
-                </div>
-            </div>
-            <div className="art-cards-content">
-                <div className="card">
-                    <img src="../images/logocircle.png" className="card-img-top" alt="404"></img>
-                    <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the
-                            bulk of the card's content.</p>
-                    </div>
-                </div>
-                <div className="card">
-                    <img src="logocircle.png" className="card-img-top" alt="404"></img>
-                    <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the
-                            bulk of the card's content.</p>
+        <div className="wrapper">
+            <div className="content">
+                <div className="homepage-welcome-content">
+                    <div className='welcome-content'>
+                        <div id='welcome' className='welcome-title'>
+                            WELCOME
+                        </div>
+                        <div id='welcome' className="welcome-subtitle">
+                            We are a project that combines an online gallery with an NFT-marketplace. The mission of
+                            this
+                            project is to create a singular void space for viewing and exchanging your experience and
+                            observations, concepts and ideas, through the form of virtual artwork as it is, in our
+                            opinion
+                            the only singular thing that won't perish as long as the humankind exists. we hope you'll be
+                            able to find an artwork that may spark a creative idea inside of your mind.
+                        </div>
                     </div>
                 </div>
-                <div className="card">
-                    <img src="logocircle.jpg" className="card-img-top" alt="404"></img>
-                    <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the
-                            bulk of the card's content.</p>
+                <div className="art-cards-wrapper">
+                    <div className="art-cards-content">
+                        <div className="card">
+                            <video class="card-img-top" width="320" height="240" controls>
+                                <source src="https://i.imgur.com/14rlaPj.mp4" type="video/mp4"/>
+                            </video>
+                            <div className="card-body">
+                                <p className="card-text">Some quick example text to build on the card title and make up
+                                    the
+                                    bulk of the card's content.</p>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="card-img">
+                                <img src="https://i.imgur.com/jufSf7j.jpeg" className="card-img-top" alt="404"></img>
+                            </div>
+                            <div className="card-body">
+                                <p className="card-text">Some quick example text to build on the card title and make up
+                                    the
+                                    bulk of the card's content.</p>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="card-img">
+                                <img src="https://i.imgur.com/sSGAmAy.gif" className="card-img-top" alt="404"></img>
+                            </div>
+                            <div className="card-body">
+                                <p className="card-text">Some quick example text to build on the card title and make up
+                                    the
+                                    bulk of the card's content.</p>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="card-img">
+                                <img src="https://i.imgur.com/JcsVhag.jpeg" className="card-img-top" alt="404"></img>
+                            </div>
+                            <div className="card-body">
+                                <p className="card-text">Some quick example text to build on the card title and make up
+                                    the
+                                    bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="art-cards-content">
+                        <div className="card">
+                            <video className="card-img-top" width="320" height="240" controls>
+                                <source src="https://i.imgur.com/14rlaPj.mp4" type="video/mp4"/>
+                            </video>
+                            <div className="card-body">
+                                <p className="card-text">Some quick example text to build on the card title and make up
+                                    the
+                                    bulk of the card's content.</p>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="card-img">
+                                <img src="https://i.imgur.com/jufSf7j.jpeg" className="card-img-top" alt="404"></img>
+                            </div>
+                            <div className="card-body">
+                                <p className="card-text">Some quick example text to build on the card title and make up
+                                    the
+                                    bulk of the card's content.</p>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="card-img">
+                                <img src="https://i.imgur.com/sSGAmAy.gif" className="card-img-top" alt="404"></img>
+                            </div>
+                            <div className="card-body">
+                                <p className="card-text">Some quick example text to build on the card title and make up
+                                    the
+                                    bulk of the card's content.</p>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="card-img">
+                                <img src="https://i.imgur.com/JcsVhag.jpeg" className="card-img-top" alt="404"></img>
+                            </div>
+                            <div className="card-body">
+                                <p className="card-text">Some quick example text to build on the card title and make up
+                                    the
+                                    bulk of the card's content.</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="card">
-                    <img src="logocircle.jpg" className="card-img-top" alt="404"></img>
-                    <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the
-                            bulk of the card's content.</p>
-                    </div>
-                </div>
+
             </div>
 
-            <footer class="page-footer font-small blue pt-4 bg-secondary">
-                <div class="container-fluid text-center text-md-left">
-                    <div class="row">
-                        <div class="col-md-6 mt-md-0 mt-3">
-                            <h5 class="text-uppercase">Footer Content</h5>
+            <footer className="page-footer font-small blue pt-4 bg-secondary">
+                <div className="container-fluid text-center text-md-left">
+                    <div className="row">
+                        <div className="col-md-6 mt-md-0 mt-3">
+                            <h5 className="text-uppercase">Footer Content</h5>
                             <p>Here you can use rows and columns to organize your footer content.</p>
                         </div>
-                        <hr class="clearfix w-100 d-md-none pb-3"/>
-                            <div class="col-md-3 mb-md-0 mb-3">
+                        <hr className="clearfix w-100 d-md-none pb-3"/>
+                        <div className="col-md-3 mb-md-0 mb-3">
 
-                                <h5 class="text-uppercase">Links</h5>
+                            <h5 className="text-uppercase">Powered by</h5>
 
-                                <ul class="list-unstyled">
-                                    <li>
-                                        <a href="#!">Link 1</a>
-                                    </li>
-                                    <li>
-                                        <a href="#!">Link 2</a>
-                                    </li>
-                                    <li>
-                                        <a href="#!">Link 3</a>
-                                    </li>
-                                    <li>
-                                        <a href="#!">Link 4</a>
-                                    </li>
-                                </ul>
+                            <ul className="list-unstyled">
+                                <li>
+                                    <a className="pow-by-icons" href="https://ru.reactjs.org">Bootstrap</a>
+                                </li>
+                                <li>
+                                    <a className="pow-by-icons" href="https://getbootstrap.com">React</a>
+                                </li>
+                                <li>
+                                    <a className="pow-by-icons" href="https://react-icons.github.io/react-icons">React
+                                        Icon</a>
+                                </li>
+                            </ul>
 
-                            </div>
-                            <div class="col-md-3 mb-md-0 mb-3">
-                                <h5 class="text-uppercase">Links</h5>
-                                <ul class="list-unstyled">
-                                    <li>
-                                        <a href="#!">Link 1</a>
-                                    </li>
-                                    <li>
-                                        <a href="#!">Link 2</a>
-                                    </li>
-                                    <li>
-                                        <a href="#!">Link 3</a>
-                                    </li>
-                                    <li>
-                                        <a href="#!">Link 4</a>
-                                    </li>
-                                </ul>
+                        </div>
+                        <div className="col-md-3 mb-md-0 mb-3">
+                            <h5 className="text-uppercase">Links</h5>
+                            <ul className="list-unstyled">
+                                <li>
+                                    <a href="#!">Link 1</a>
+                                </li>
+                                <li>
+                                    <a href="#!">Link 2</a>
+                                </li>
+                                <li>
+                                    <a href="#!">Link 3</a>
+                                </li>
+                                <li>
+                                    <a href="#!">Link 4</a>
+                                </li>
+                            </ul>
 
-                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="footer-copyright text-center py-3">© 2021 Copyright:
+                <div className="footer-copyright text-center py-3">© 2021 Copyright:
                     <a href="https://www.instagram.com/cpt_prod/"> inst: @capt_prod</a>
                 </div>
             </footer>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,24 +1,43 @@
 import React from "react";
 import "./NavBar.css"
 import {Link} from "react-router-dom";
+import { FiSearch } from 'react-icons/fi';
 
 
 
-export let NavBar = () => {
+export let NavBar = (props) => {
+
     return (
-        <div>
+        <div className="nav-bar">
             <nav className="navbar navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <form className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button className="btn btn-light" type="submit">Search</button>
-                    </form>
-                    <div className="nav-item">
-                       THE SPACE OF NOWHERE
+                    <div className="nav-search-button">
+                        <nav className="navbar navbar-dark bg-dark">
+                            <div className="container-fluid">
+                                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                        data-bs-target="#navbarToggleExternalContent"
+                                        aria-controls="navbarToggleExternalContent" aria-expanded="false"
+                                        aria-label="Toggle navigation">
+                                    <FiSearch size={32} />
+                                </button>
+                            </div>
+                        </nav>
+                        <div className="search-bar">
+                            <div className="collapse" id="navbarToggleExternalContent">
+                                <form className="d-flex">
+                                    <input className="form-control me-2" type="search" placeholder="Search"
+                                           aria-label="Search"/>
+                                    <button className="btn btn-light" type="submit">Search</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='navbar-logo'>
+                        <img className="logo-navigation" src="https://i.imgur.com/esaMyUF.png"/>
                     </div>
                     <div className="navbar-links">
                         <ul className="nav nav-pills">
-                            <Link className="nav-link" to="/homepage">HomePage</Link>
+                            <Link className="nav-link" to="/homepage">Home </Link>
                             <Link className="nav-link" to="/contacts">Contacts</Link>
                             <Link className="nav-link" to="/about">About</Link>
                         </ul>
@@ -27,7 +46,7 @@ export let NavBar = () => {
             </nav>
         </div>
 
-);
+    );
 }
 
 
